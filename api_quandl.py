01f09e0ded083ffada7794a02e1731f45c20167b
dmichie09230214 <64871280+dmichie09230214@users.noreply.github.com>
2020-06-02 15:11:17 -0400

    Update api_quandl.py

--- api_quandl.py
+++ api_quandl.py
@@ -9,3 +9,8 @@ w = quandl.get('EOD/DIS', start_date="2016-12-01", end_date="2017-12-30")
 
 plt.plot(w)
 plt.show()
+
+#user will need to register at the quandl site
+#once register, user is provided with an API that is needed. 
+#program is created and perform by PyCharm
+#Quandl will need to be downloaded by running pip install quandl
